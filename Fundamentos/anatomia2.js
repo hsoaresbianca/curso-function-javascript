46486f16058c812b1c6687fffb26bd68a420bff0
Bianca Soares <devbiancasoares@gmail.com>
2026-07-27 18:15:14 -0300

    Anatomia da função - aula 2
    função anônima e expressão de função.

---
+++ Fundamentos/anatomia2.js
@@ -0,0 +1,12 @@
+// Function Anonymus - Função Anônima
+// função sem nome
+(function (a, b, c){
+    return a + b + c
+})
+
+
+// Function Expression - Expressão de Função
+// cria uma variável (var, let ou const) e atribui (=) uma função anonima.
+const soma = function (a, b){return a + b}
+
+console.log(soma(10, 25))
